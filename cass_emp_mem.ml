8b4dbaf51883b1b5d26e0223a76acf35d1e1e12b
shashank <shashank.shekhardubey@gmail.com>
2019-09-04 23:15:16 +0530

    modified file

--- cass_emp_mem.ml
+++ cass_emp_mem.ml
@@ -1,9 +1,6 @@
 #require "lwt.unix";;
 #require "irmin";;
 #require "irmin-mem";;
-#require "lwt.unix";;
-#require "irmin";;
-#require "irmin-mem";;
 
 
 open Lwt_main;;
@@ -11,21 +8,24 @@ open Irmin;;
 open Irmin_mem;;
 
 module Store_module = Irmin_mem.AO (Irmin.Hash.SHA1) (Irmin.Contents.String);;
-module RW_module = Irmin_mem.RW (Irmin.Contents.String) (Irmin.Contents.String);;
+module RW_module = Irmin_mem.RW (Irmin.Contents.String) (Irmin.Hash.SHA1);;
 
 let config = Irmin_mem.config () ;;
 let hashtable = Lwt_main.run @@ Store_module.v config ;;
 
-let key = Lwt_main.run  (Store_module.add hashtable "2 emp2 dept2") ;;
+let key = Lwt_main.run  (Store_module.add hashtable "emp3") ;;
 
 let aw_t = Lwt_main.run @@ RW_module.v config ;;
 
-(* let cstruct = Irmin.Hash.SHA1.to_raw key ;; *)
-let cstruct = Irmin.Hash.SHA1.to_raw_int key ;;
-let str = Cstruct.to_string cstruct ;;
-let key2 = String.sub str 8 ((String.length str) - 8) ;;
+let _ = RW_module.set aw_t "branch2" key ;;
 
-let _ = RW_module.set aw_t "master" key2 ;;
+
+(* let cstruct = Irmin.Hash.SHA1.to_raw key ;; *)
+(* let cstruct = Irmin.Hash.SHA1.to_raw_int key ;;
+let str = Cstruct.to_string cstruct ;;
+let key2 = String.sub str 8 ((String.length str) - 8) ;; *)
+
+
 
 
 
